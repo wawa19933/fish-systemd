1dbe9ead17461bae9990a61dd4ac54dcd93d8d9e
Sergio Scalpone <wawa19933@gmail.com>
2019-09-02 00:49:17 +0200

    Add abbreviations and smart function

---
+++ conf.d/systemd-abbr.fish
@@ -0,0 +1,4 @@
+abbr -a -g sc systemctl
+abbr -a -g scu systemctl --user
+abbr -a -g jor journalctl
+abbr -a -g jour journalctl --user
---
+++ systemctl.fish
@@ -0,0 +1,13 @@
+function systemctl -d 'wraps privileged and user systemctl commands' -w systemctl
+    set -l user_commands \
+      list-units list-unit-files list-jobs list-timers \
+      list-sockets list-dependencies list-machines \
+      is-active is-enabled is-failed \
+      status show help get-default show-environment cat 
+
+    if contains -- --user $argv; or contains $argv[1] $user_commands
+        command systemctl $argv
+    else
+        command sudo systemctl $argv
+    end
+end
