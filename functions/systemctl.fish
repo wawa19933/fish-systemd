6b55727f301037b626ce6c4709bd0a9b7bd3e9b0
Sergii Maksymov <maksymov@dexor.io>
2021-05-20 12:54:58 +0200

    Fix issue with unnecessary sudo

--- functions/systemctl.fish
+++ functions/systemctl.fish
@@ -1,13 +1,22 @@
-function systemctl -d 'wraps privileged and user systemctl commands' -w systemctl
+# Defined in /tmp/fish.AOswl4/systemctl.fish @ line 2
+function systemctl --description 'wraps privileged and user systemctl commands'
     set -l user_commands \
       list-units list-unit-files list-jobs list-timers \
       list-sockets list-dependencies list-machines \
       is-active is-enabled is-failed \
-      status show help get-default show-environment cat 
+      status show help get-default show-environment cat
 
-    if contains -- --user $argv; or contains -- $argv[1] $user_commands
+    set -l root_commands start stop reload restart try-restart \
+        reload-or-restart try-reload-or-restart isolate kill clean \
+        set-property reset-failed enable disable reenable preset \
+        preset-all mask unmask link revert add-wants add-requires \
+        edit set-default cancel set-environment unset-environment import-environment \
+        daemon-reload daemon-reexec default rescue emergency halt poweroff reboot \
+        kexec exit switch-root suspend hibernate hybrid-sleep suspend-then-hibernate
+
+    if contains -- --user $argv; or not contains -- $argv[1] $root_commands
         command systemctl $argv
     else
-        command sudo systemctl $argv
+        sudo systemctl $argv
     end
 end
